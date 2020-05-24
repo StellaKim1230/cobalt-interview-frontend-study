6350a394c00cb1f4798bde59d167e9fca9ec7ab5
Stellakim1230 <stellakim1230@gmail.com>
2020-05-24 18:28:44 +0900

    delete TableHeader, TableRow file.

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,7 +1,8 @@
 import React, { FC, useState } from 'react'
 
-import TableHeader from './TableHeader'
-import TableRow from './TableRow'
+import cx from 'classnames'
+
+import { get } from 'lodash'
 
 import { TableColumn } from '../@types/model'
 
@@ -23,6 +24,14 @@ const Table: FC<Props> = ({
   expandableKey,
 }) => {
   const [ isExpandable, setIsExpandable ] = useState(false)
+  const [ isShowingExpandableContent, setIsShowingExpandableContent ] = useState(false)
+  const [ expandTdId, setExpandTdId ] = useState('')
+
+  const handleClickExpandTd = (e: React.MouseEvent, id: string) => {
+    setIsShowingExpandableContent(!isShowingExpandableContent)
+    setExpandTdId(id)
+  }
+
   return (
     <>
       <h2 className="Table__title">{title}</h2>
@@ -38,16 +47,57 @@ const Table: FC<Props> = ({
         </>
       )}
       <table className="Table">
-        <TableHeader
-          columns={columns}
-          isExpandable={isExpandable}
-        />
-        <TableRow
-          columns={columns}
-          dataSource={dataSource}
-          isExpandable={isExpandable}
-          expandableKey={expandableKey}
-        />
+        <thead className="TableHeader">
+          <tr className="TableHeader__tr">
+            {isExpandable && (
+              <th className="TableHeader__th"></th>
+            )}
+            {columns.map(({ key, title, className, style }) => (
+              <th
+                key={key}
+                className={cx('TableHeader__th', className)}
+                style={style}
+              >
+                {title}
+              </th>
+            ))}
+          </tr>
+        </thead>
+        <tbody className="TableRow">
+          {dataSource.map(d => (
+            <>
+              <tr className="TableRow__tr" key={d.id}>
+                {isExpandable && (
+                  <td
+                    className={cx('TableRow__td', {
+                      'TableRow__td--expandable': isExpandable,
+                    })}
+                    onClick={(e) => handleClickExpandTd(e, d.id)}
+                  >
+                    <img
+                      src="/icons/arrow.png"
+                      alt="arrow icon"
+                    />
+                  </td>
+                )}
+                {columns.map(({ dataIndex, render, style }) => (
+                  <td
+                    className="TableRow__td"
+                    key={dataIndex}
+                    style={style}
+                  >
+                    {render ? render(get(d, dataIndex)) : get(d, dataIndex)}
+                  </td>
+                ))}
+              </tr>
+              {isShowingExpandableContent && expandTdId === d.id && (
+                <tr className="TableRow__tr">
+                  <td colSpan={Object.keys(d).length + 1}>{expandableKey ? get(d, expandableKey) : 'no content'}</td>
+                </tr>
+              )}
+            </>
+          ))}
+        </tbody>
       </table>
     </>
   )
